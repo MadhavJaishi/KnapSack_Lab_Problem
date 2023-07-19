ab7f77152f2e6f0fc6edb08437898deb5f043032
MadhavJaishi <108451297+MadhavJaishi@users.noreply.github.com>
2023-07-19 21:17:34 +0530

    Update KnacksackUsingGreedyTechniq02.java

--- KnacksackUsingGreedyTechniq.java
+++ KnacksackUsingGreedyTechniq.java
@@ -1,29 +1,30 @@
+import java.util.Arrays;
 import java.util.Scanner;
 class KnacksackUsingGreedyTechniq
 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int object, m;
-        System.out.println("Enter the Total Objects");
-        object = sc.nextInt();
-        int[] weight = new int[object];
-        int[] profit = new int[object];
-        int[] itemno = new int[object];
-        for(int i = 0; i < object; i++)
+        int n, m;
+        System.out.println("Enter the no. of items: ");
+        n = sc.nextInt();
+        int[] weight = new int[n];
+        int[] profit = new int[n];
+        int[] items = new int[n];
+        for(int i = 0; i < n; i++)
         {
-            System.out.println("Enter the Profit");
+            System.out.println("Enter Profit: ");
             profit[i] = sc.nextInt();
-            System.out.println("Enter the weight");
+            System.out.println("Enter weight: ");
             weight[i] = sc.nextInt();
-            itemno[i] = i + 1;
+            items[i] = i + 1;
 
         }
-        System.out.println("Enter the Knapsack capacity");
+        System.out.println("Enter the Knapsack capacity: ");
         m = sc.nextInt();
 
-        double[] p_w = new double[object];
-        for(int i = 0; i < object; i++)
+        double[] p_w = new double[n];
+        for(int i = 0; i < n; i++)
         {
             p_w[i] = (double)profit[i] / (double)weight[i];
         }
@@ -32,32 +33,32 @@ class KnacksackUsingGreedyTechniq
         System.out.println("-----Data-Set------");
         System.out.print("-------------------");
         System.out.println();
-        System.out.print("Objects");
-        for(int i = 1; i <= object; i++)
+        System.out.print("Items: ");
+        for(int i = 1; i <= n; i++)
         {
             System.out.print(i + "    ");
         }
         System.out.println();
-        System.out.print("Profit ");
-        for(int i = 0; i < object; i++)
+        System.out.print("Profit: ");
+        for(int i = 0; i < n; i++)
         {
             System.out.print(profit[i] + "    ");
         }
         System.out.println();
-        System.out.print("Weight ");
-        for(int i = 0; i < object; i++)
+        System.out.print("Weight: ");
+        for(int i = 0; i < n; i++)
         {
             System.out.print(weight[i] + "    ");
         }
         System.out.println();
-        System.out.print("P/W    ");
-        for(int i = 0; i < object; i++)
+        System.out.print("P/W: ");
+        for(int i = 0; i < n; i++)
         {
             System.out.print(p_w[i] + "  ");
         }
-        for(int i = 0; i < object - 1; i++)
+        for(int i = 0; i < n - 1; i++)
         {
-            for(int j = i + 1; j < object; j++)
+            for(int j = i + 1; j < n; j++)
             {
                 if(p_w[i] < p_w[j])
                 {
@@ -73,9 +74,9 @@ class KnacksackUsingGreedyTechniq
                     weight[j] = weight[i];
                     weight[i] = temp2;
 
-                    int temp3 = itemno[j];
-                    itemno[j] = itemno[i];
-                    itemno[i] = temp3;
+                    int temp3 = items[i];
+                    items[i] = items[j];
+                    items[j] = temp3;
 
                 }
             }
@@ -85,29 +86,30 @@ class KnacksackUsingGreedyTechniq
         System.out.println("--After Arranging--");
         System.out.print("-------------------");
         System.out.println();
-        System.out.print("Objects");
-        for(int i = 1; i <= object; i++)
+        System.out.print("Items: ");
+        for(int i = 0; i < n; i++)
         {
-            System.out.print(i + "    ");
+            System.out.print(items[i] + "    ");
         }
         System.out.println();
-        System.out.print("Profit ");
-        for(int i = 0; i < object; i++)
+        System.out.print("Profit: ");
+        for(int i = 0; i < n; i++)
         {
             System.out.print(profit[i] + "    ");
         }
         System.out.println();
-        System.out.print("Weight ");
-        for(int i = 0; i < object; i++)
+        System.out.print("Weight: ");
+        for(int i = 0; i < n; i++)
         {
             System.out.print(weight[i] + "    ");
         }
         System.out.println();
-        System.out.print("P/W    ");
-        for(int i = 0; i < object; i++)
+        System.out.print("P/W: ");
+        for(int i = 0; i < n; i++)
         {
             System.out.print(p_w[i] + "  ");
         }
+        int[] selectedItems = new int[n];
         int k = 0;
         double sum = 0;
         System.out.println();
@@ -117,18 +119,19 @@ class KnacksackUsingGreedyTechniq
             {
                 sum += profit[k];
                 m = m - weight[k];
+                selectedItems[k] = items[k];
             }
             else
             {
-                double temp = (m * profit[k]) / weight[k];
+                double temp = (double) (m * profit[k]) / weight[k];
                 sum = sum + temp;
+                selectedItems[k] = items[k];
                 m = 0;
             }
-            selectedItems[k] = itemno[k];
             k++;
         }
         System.out.println("Final Profit is=" + sum);
         System.out.println("Items considered are: ");
-        System.out.print(selectedItems);
+        System.out.print(Arrays.toString(selectedItems));
     }
 }
